a8726295e838084c7a502556c56c6153feba8f5d
Sazuna <liza.fretel@gmail.com>
2026-02-27 15:09:02 +0100

    Do not generate definitions anymore

--- src/uat_to_corpus.py
+++ src/uat_to_corpus.py
@@ -60,9 +60,9 @@ def main(uat_file: pathlib.Path):
         if not SKOS.prefLabel in values:
             del description_by_uat[uat]
             continue
-        if not SKOS.definition in values:
-            definition = augment_definition(str(values[SKOS.prefLabel][0]), key = str(uat))
-            values[SKOS.definition] = [definition]
+        #if not SKOS.definition in values:
+        #    definition = augment_definition(str(values[SKOS.prefLabel][0]), key = str(uat))
+        #    values[SKOS.definition] = [definition]
 
 
 
