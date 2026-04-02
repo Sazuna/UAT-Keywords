3ae2f1f4cb8dd3a7396e3a6572660fdb880bb811
Sazuna <liza.fretel@gmail.com>
2026-04-02 17:16:16 +0200

    TF-IDF inference print fix

--- src/TF-IDF/main.py
+++ src/TF-IDF/main.py
@@ -1,8 +1,9 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from tqdm import tqdm
 from typing import List, Tuple, Dict
-from rdflib import Graph, RDF, SKOS
+from rdflib import Graph, URIRef, RDF, SKOS
 from corpus_loader import Reader
 from config import ADS_CORPUS_DIR
 
@@ -15,12 +16,42 @@ from sklearn.decomposition import TruncatedSVD
 
 
 # Hyperparameters
-MAX_FEATURES: int = 30000
-INPUT_DIM: int    = 512#2048
-MAX_ITER: int     = 3000
+MAX_FEATURES: int = 100000 # tf-idf vectorizer max features
+INPUT_DIM: int    = 8192 # input dim of the classifier (truncatedSVD will select N features from the above max_features)
+EPOCHS: int       = 10
+TOP_K: int        = 10
 batch_size: int   = 64
 
 stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+# 0. Load ontology
+def load_nodes(rdf) -> Tuple[Dict, Dict]:
+
+    # 1. Collect all nodes (subjects or objects of a SKOS relation)
+    nodes_set = set()
+    for s, _, _ in rdf.triples((None, RDF.type, SKOS.Concept)):
+        s_str = str(s)
+        nodes_set.add(s_str)
+
+    # 2. Node indexation
+    sorted_nodes = sorted(nodes_set, key = lambda x: int(x.split('/')[-1]))
+    node2idx = {n: i for i, n in enumerate(sorted_nodes)}
+    idx2node = {i: n for n, i in node2idx.items()}
+    return node2idx, idx2node
+
+def load_labels(node2idx,
+                rdf: Graph) -> Dict:
+    uri2label = dict()
+    for uri in node2idx.keys():
+        print(uri)
+        for _, _, label in rdf.triples((URIRef(uri), SKOS.prefLabel, None)):
+            uri2label[str(uri)] = str(label)
+    return uri2label
+
+rdf = Graph()
+rdf.parse("../corpus/UAT_v6.0.0.rdf")
+node2idx, idx2node = load_nodes(rdf)
+node2label = load_labels(node2idx, rdf)
 
 # 1. Load corpus
 reader = Reader()
@@ -33,21 +64,6 @@ for text, uat in reader.read_corpus(ignore_kailas=False,
     uats.append(uat)
 
 ### Generate multihot
-def load_nodes(uat_ontology_path) -> Tuple[Dict, Dict]:
-    rdf = Graph()
-    rdf.parse(uat_ontology_path, format="xml")
-
-    # 1. Collect all nodes (subjects or objects of a SKOS relation)
-    nodes_set = set()
-    for s, _, _ in rdf.triples((None, RDF.type, SKOS.Concept)):
-        s_str = str(s)
-        nodes_set.add(s_str)
-
-    # 2. Node indexation
-    sorted_nodes = sorted(nodes_set, key = lambda x: int(x.split('/')[-1]))
-    node2idx = {n: i for i, n in enumerate(sorted_nodes)}
-    idx2node = {i: n for n, i in node2idx.items()}
-    return node2idx, idx2node 
 def build_multihot(
     annotations: List[List[str]],
     node2idx: Dict[str, int],
@@ -77,7 +93,6 @@ def build_multihot(
                 print(f"[Warning] Unknown URI in node2idx : {uri}")
     return multihot
 
-node2idx, idx2node = load_nodes("../corpus/UAT_v6.0.0.rdf")
 multihot = build_multihot(annotations=uats,
                           node2idx=node2idx)
 
@@ -85,6 +100,7 @@ multihot = build_multihot(annotations=uats,
 #    return regex.findall(r"[\w]+", text)
 
 # 2. Learn TF-IDF
+print("Training TF-IDF vectorizer...")
 vectorizer = TfidfVectorizer(lowercase=True,
                              stop_words=stop_words,
                              ngram_range=(1, 3),
@@ -92,9 +108,11 @@ vectorizer = TfidfVectorizer(lowercase=True,
                              dtype=np.float32)
 
 vectorizer = vectorizer.fit(texts)
+print("Done")
 
 # 3. Transform texts
 
+print("Transforming text to features...")
 X = vectorizer.transform(texts)
 Y = multihot
 
@@ -102,57 +120,7 @@ Y = multihot
 svd = TruncatedSVD(n_components=INPUT_DIM, random_state=42)
 svd = svd.fit(X)
 X = svd.fit_transform(X)
-
-# 4. Logistic regression
-
-### 2411 classifications (very slow) ###
-"""
-model = OneVsRestClassifier(
-    LogisticRegression(
-        solver="saga",        # Sparse + scalable
-        class_weight="balanced",
-        max_iter=MAX_ITER,
-        n_jobs=-1,
-    ),
-    verbose=1,
-)
-model.fit(X, Y)
-"""
-"""
-model = OneVsRestClassifier(
-    SGDClassifier(
-        loss="log_loss",
-        max_iter=MAX_ITER,
-        verbose=1,
-        n_jobs=-1
-    )
-)
-model.fit(X, Y)
-
-# 5. Inference
-text_test = []
-uats_test = []
-for text, uat in reader.read_pre9forADS():
-    text_test.append(text)
-    uats_test.append(uat)
-
-X_test = vectorizer.transform(text_test)
-
-Y_proba = model.predict_proba(X_test)  # shape [D_test, 2411]
-
-def top_k_predictions(Y_proba, k=5):
-    Y_pred = np.zeros_like(Y_proba)
-    for i in range(Y_proba.shape[0]):
-        top_k = np.argsort(Y_proba[i])[-k:]
-        Y_pred[i, top_k] = 1
-    return Y_pred
-
-Y_pred = top_k_predictions(Y_proba, k=5)
-
-print("Micro F1:", f1_score(uats_test, Y_pred, average="micro"))
-print("Macro F1:", f1_score(uats_test, Y_pred, average="macro"))
-
-"""
+print("Done")
 
 # 4. Model
 class Model(nn.Module):
@@ -176,7 +144,8 @@ criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
 n_samples = X.shape[0]
-for epoch in range(10):
+print("Training linear regression model...")
+for epoch in tqdm(range(EPOCHS)):
     total_loss = 0.0
 
     for i in range(0, n_samples, batch_size):
@@ -196,6 +165,7 @@ for epoch in range(10):
         total_loss += loss.item()
     
     print(f"Epoch {epoch}, loss={total_loss:.4f}")
+print("Done")
 
 # 5. Inference
 text_test = []
@@ -212,11 +182,14 @@ print(X_test_tensor.shape)
 logits = model(X_test_tensor)
 probs = torch.sigmoid(logits)
 preds = (probs > 0.1).int()
-top_k = torch.topk(probs, k=10, dim=1)
+top_k = torch.topk(probs, k=TOP_K, dim=1)
 print(top_k)
-print(idx2node.keys())
-for idx, (text, uats) in zip(top_k.indices, zip(text_test, uats_test)):
+for idx, (scores, (text, uats)) in zip(top_k.indices, zip(top_k.values, zip(text_test, uats_test))):
     predicted_uats = [idx2node[int(uat)] for uat in idx]
+    uats_labels = [node2label.get(uat, "UNKNOWN") for uat in uats]
+    predicted_uats_labels = [node2label.get(uat, "UNKNOWN") for uat in predicted_uats]
     print("Text:", text[:512])
-    print("paper UATs:", ' '.join(uats))
-    print("predicted UATs:", ' '.join(predicted_uats))
+    print("paper UATs:", '\n\t'.join([f"{uat} ({label})" for uat, label in zip(uats, uats_labels)]))
+    #print("predicted UATs:", ' '.join(predicted_uats))
+    print("predicted UATs:", '\n\t'.join([f"{score:.4f}: {uat} ({label})" for score, (uat, label) in zip(scores, zip(predicted_uats, predicted_uats_labels))]))
+    print("")
